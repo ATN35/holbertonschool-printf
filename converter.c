d2d4c60ad3d5292909ecb8868c5ab34b7c170462
ATN35 <contact.atn35@gmail.com>
2023-08-30 07:13:41 -0700

    Pourcentage

---
+++ converter.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_percent - pourcentage
+ * Description: description
+ * @list: variable
+ * Return: retourne la longueur
+ */
+int print_percent(va_list list)
+{
+	(void)list;
+	return (_putchar('%'));
+}
