90ccb4e068b18690de49640e72b8f97eb5e8ccd9
ATN35 <contact.atn35@gmail.com>
2023-08-30 06:07:15 -0700

    Print all types

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _printf - Print all types
+ * @format: first parameter
+ * Return: Print all types
+ */
+int _printf(const char *format, ...)
+{
+	elem_t array[] = {
+		{"c", character},
+		{"s", strings},
+		{"i", integer},
+		{"d", decimal},
+		{"%", percent},
+		{"u", unsigned_int},
+		{NULL, NULL},
+	};
+	va_list list;
+	int i = 0, j;
+	int lenght = 0;
+
+	va_start(list, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (array[j].element != NULL)
+			{
+				if (format[i + 1] == *(array[j].element))
+				{
+					lenght += array[j].f(list);
+					i += 2;
+					break;
+				}
+				j++;
+			}
+		}
+		_putchar(format[i]);
+		lenght++;
+		i++;
+	}
+	va_end(list);
+	return (lenght);
+}
